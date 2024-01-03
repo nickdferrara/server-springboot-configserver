cd4ffccf6b279fd560a7743ef8ca76d42a69d9c6
nickdferrara <nick.d.ferrara@gmail.com>
2024-01-02 21:43:54 -0500

    Inital Check In

---
+++ src/test/kotlin/com/nickdferrara/configserver/ServerSpringbootConfigserverApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nickdferrara.configserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServerSpringbootConfigserverApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
